6d6ad4ba12bd6e5019377cf475999119769c4f9d
Rhyan Inacio <rhyanalves68@gmail.com>
2021-03-11 14:48:04 -0300

    comentarios em sketch_led.ino

--- sketch_led/sketch_led.ino
+++ sketch_led/sketch_led.ino
@@ -1,28 +1,24 @@
-int statusLed = 0;
-const int LED = 12;
+int statusLed = 0; //LIGADO OU DESLIGADO
+const int LED = 12; //PINO LED
 
 void setup() {
 
-  Serial.begin(9600);
-  pinMode(LED, OUTPUT);
-  digitalWrite(LED, LOW);
+  Serial.begin(9600); //taxa de tranferencia
+  pinMode(LED, OUTPUT); //pino de saida
+  digitalWrite(LED, LOW); //iniciar em desligado
 
 }
 
 void loop() {
+ 
+  if (Serial.available() > 0) { //verifica se foi enviado algo pela porta
 
-
-  if (Serial.available() > 0) {
-
-    statusLed = Serial.read();
+    statusLed = Serial.read(); //ler o que foi enviado pela porta serial
     if (statusLed == '0') {
       digitalWrite(LED, LOW);
-
     } else if (statusLed == '1') {
       digitalWrite(LED, HIGH);
-
     }
-
   }
 
 }
